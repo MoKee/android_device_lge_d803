b3adf1653e25467b7087282468f3c975240497cf
john craig <johnccraig80@gmail.com>
2014-01-16 07:45:43 -0500

    make build with CM
    Change-Id: I93b4f6a33facc6bb11974a31a60805725534618c

---
+++ cm.mk
@@ -0,0 +1,23 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/common_gsm.mk)
+
+## Inherit some common cyaogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d803/d803.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d803
+PRODUCT_NAME := cm_d803
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D803
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_tls_ca/g2:4.2.2/JDQ39B/D803T10b.1378018720:user/release-keys PRIVATE_BUILD_DESC="g2_tls_ca-user 4.2.2 JDQ39B D803T10b.1378018720 release-keys"
+
+PRODUCT_PACKAGES += Torch
